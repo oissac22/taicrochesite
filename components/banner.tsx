db556dd9211e52a64c4ab99652dab8c94598c7b1
unknown <oissac22@hotmail.com>
2021-05-29 14:40:18 -0300

    banner finish 1

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import configSystem from '../config';
 
 
 const BANNER_START = '/imgs/banners/bannerStart.jpg'
+const DESFOQUE = '5px';
+const VIEW_HEIGHT = '70vh'
+const COLOR_TEXT = 'white'
+const FAIXA_BG_COLOR = 'rgba(0,0,0,0.7)'
 
 
 
@@ -14,20 +19,62 @@ const BannerStart: React.SFC<BannerStartProps> = () => {
             <style jsx>
                 {`
                     .container{
+                        display:grid;
+                        grid-template-columns:1fr;
+                        grid-template-rows:1fr;
+                    }
+                    .fundo{
+                        grid-column:1/2;
+                        grid-row:1/2;
                         background-image:url(${BANNER_START});
                         background-size:cover;
-                        height:100vh;
+                        height:${VIEW_HEIGHT};
+                        -webkit-filter: blur(${DESFOQUE});
+                        -moz-filter: blur(${DESFOQUE});
+                        -o-filter: blur(${DESFOQUE});
+                        -ms-filter: blur(${DESFOQUE});
+                        filter: blur(${DESFOQUE});
+                    }
+                    .frente{
+                        grid-column:1/2;
+                        grid-row:1/2;
+                        z-index:2;
                         background-position:center;
-                        -webkit-filter: blur(15px);
-                        -moz-filter: blur(15px);
-                        -o-filter: blur(15px);
-                        -ms-filter: blur(15px);
-                        filter: blur(15px);
+                        display:flex;
+                        align-items:center;
+                        justify-content:center;
+                    }
+                    .texts{
+                        font-size:1rem;
+                        text-align:center;
+                        color:${COLOR_TEXT};
+                        text-shadow: 3px 3px rgba(0,0,0,0.4);
+                        display:flex;
+                        flex-direction:column;
+                        gap:15px;
+                        background-color: ${FAIXA_BG_COLOR};
+                        padding:2rem;
+                        border-radius:6px;
+                    }
+                    .text1{
+                        font-size:3rem;
+                        font-weight:bold;
+                    }
+                    .text2{
+                        font-size:2rem;
+                    }
+                    .text3{
+                        font-size:1.5rem;
                     }
                 `}
             </style>
-            <div>
-
+            <div className='fundo' />
+            <div className='frente'>
+                <div className='texts'>
+                    <div className='text1'>Sorteio de vários Amigurumi</div>
+                    <div className='text2'>São 32 peças de uma vez</div>
+                    <div className='text3'>Valor de R$ 20,00</div>
+                </div>
             </div>
         </div>
 }
---
+++ config.ts
@@ -0,0 +1,17 @@
+
+
+const configSystem = {
+
+
+    bannerText: {
+        text1: 'Sorteio de vários Amigurumi',
+        text2: 'São 32 peças de uma vez',
+        text3: 'Valor de R$ 20,00'
+    }
+
+
+}
+
+
+
+export default configSystem;
